dd41f5e6b8cf14e3a71b2b77c2bf1da656f51101
Ju Hyein <80535817+juhyein@users.noreply.github.com>
2023-01-19 15:12:09 +0900

    Create 햄버거 만들기.py

---
+++ juhyein/햄버거 만들기.py
@@ -0,0 +1,10 @@
+def solution(ingredient):
+    s = []
+    cnt = 0
+    for i in ingredient:
+        s.append(i)
+        if s[-4:] == [1, 2, 3, 1]:
+            cnt += 1
+            for i in range(4):
+                s.pop()
+    return cnt
