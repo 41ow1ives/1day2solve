bdd2e83691db52f81944de77ae3a8ec0d3b12cbb
kyounghyeon <whoe99@gmail.com>
2023-02-09 17:43:08 +0900

    S3, N과 M(2), 백트래킹 | 백트래킹으로 다시 풂

--- kyounghyeon/BOJ/완전탐색/15650.py
+++ kyounghyeon/BOJ/완전탐색/15650.py
@@ -2,13 +2,24 @@
 # 분류 : 백트래킹, Silver 3
 # 출처 : 백준 15650
 
-from itertools import combinations
 n, m = map(int, input().split())
+ans = []
+existed = [False] * n
 
-lst = list(range(1,n+1))
-seq = list(combinations(lst, m))
+def Comb(x):
 
-for i in seq:
-    for j in i:
-        print(j, end = " ")
-    print()
+    if len(ans) == m:
+        print(' '.join(map(str, ans)))
+        return
+
+    for i in range(x, n+1):
+        if existed[i]:
+            continue
+
+        ans.append(i)
+        existed[i] = True
+        Comb(i+1)
+        ans.pop()
+        existed[i] = False
+
+Comb(1)
