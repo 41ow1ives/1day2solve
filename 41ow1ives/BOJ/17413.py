8b293d9fa771c8f24b547b516127116df6b7083a
41ow1ives <77601251@daum.net>
2023-03-04 20:19:58 +0900

    구현: 2023.02.28

---
+++ 41ow1ives/BOJ/17413.py
@@ -0,0 +1,65 @@
+# https://www.acmicpc.net/problem/17413
+# 단어 뒤집기 2 (Silver III)
+
+import sys
+
+sentence = list(sys.stdin.readline().rstrip()) + [
+    " "
+]  # 두번째 while에서 end_idx < len(sentence) 조건 안 넣기 위해서 추가
+idx = 0
+
+while idx < len(sentence):
+
+    if sentence[idx] == "<":
+        while sentence[idx] != ">":
+            idx += 1
+        idx += 1  # 닫힌 괄호를 만났으니 +1 (안 해주면 >에서 멈춰있으니까!)
+
+    start_idx = idx
+    end_idx = idx
+
+    while sentence[end_idx] not in [" ", "<"]:  # 마지막에 공백 넣어줬으므로 태그로 끝나도 다음 글자가 공백이 됨
+        end_idx += 1
+    tmp = reversed(sentence[start_idx:end_idx])
+    sentence[start_idx:end_idx] = tmp
+    idx = end_idx
+    if (
+        sentence[end_idx] == " "
+    ):  # "<"인 경우에는 +1을 해주면 안되지만 공백인 경우에는 +1을 해줘야 다음 시작 단어로 넘어감
+        idx += 1
+
+print("".join(sentence).rstrip())
+
+"""
+####### 틀린 경우
+
+import sys
+sentence = list(sys.stdin.readline().rstrip())
+idx = 0
+flag = False
+
+while idx < len(sentence):
+
+    if sentence[idx] == "<":
+        flag = True
+        idx += 1
+    elif sentence[idx] == ">":
+        flag = False
+        idx += 1
+    elif sentence[idx] == " ":
+        idx += 1
+
+    if not flag and idx < len(sentence):
+        start_idx = idx
+        end_idx = idx
+        while end_idx < len(sentence) and sentence[end_idx] not in [" ", "<"]:
+            end_idx += 1
+        tmp = reversed(sentence[start_idx:end_idx])
+        sentence[start_idx:end_idx] = tmp
+        idx = end_idx
+    else:
+        idx += 1
+
+print("".join(sentence))
+
+"""
